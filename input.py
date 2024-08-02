205e271a916c83965396387751314dfa027aa601
RaissaAndradeS <221035077@aluno.unb.br>
2024-08-01 21:01:09 -0300

    Input

--- input.py
+++ input.py
@@ -2,3 +2,14 @@
 nome = input("Entre com seu nome: ")
 
 print(nome)
+
+# %%
+a = input("Entre com o valor de A: ")
+a = int(a)
+
+b = input("Entre com o valor de B: ")
+b = int(b)
+
+soma = a + b
+print(soma)
+
