98ac6e0a155fbcc86f56858f8773f3800a1bdc85
RaissaAndradeS <221035077@aluno.unb.br>
2024-08-01 21:07:57 -0300

    inputs basico

--- input.py
+++ input.py
@@ -10,6 +10,17 @@ a = int(a)
 b = input("Entre com o valor de B: ")
 b = int(b)
 
+# or 
+
+a = int(input("Entre com o valor A: "))
+b = int(input("Entre com o valor B: "))
+
+
 soma = a + b
 print(soma)
+
+# %%
+
+print("Raissa" + "Andrade") # RaissaAndrade
+print("10"+"30") # 1030
 
