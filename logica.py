e03ec76276f4af3fe757db28a1a8b567e992c606
RaissaAndradeS <221035077@aluno.unb.br>
2024-08-03 17:18:57 -0300

    Else

--- logica.py
+++ logica.py
@@ -4,3 +4,14 @@ idade = int(input("Entre com sua idade: "))
 
 if idade >= 18:
     print("Você é maior de idade.")
+
+
+## else 
+
+idade = int(input("Entre com a sua idade: "))
+
+if idade >= 18:
+    print("Você é maior de idade")
+
+else: 
+    print("Você é muito jovem.")
