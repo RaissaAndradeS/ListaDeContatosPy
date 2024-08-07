a05b336582ba73531cb614691d8aaa4c6495f34c
RaissaAndradeS <221035077@aluno.unb.br>
2024-08-07 15:48:44 -0300

    exercicios

---
+++ for.py
@@ -0,0 +1,12 @@
+for i in "Raissa":
+
+    if i == "R":
+        continue 
+
+    elif i == " ":
+        continue
+
+    print(i)
+
+
+## 
--- while.py
+++ while.py
@@ -19,5 +19,29 @@ while True:
 print("Saí, porra")
 
 
-## 
+## continue
 
+while True:
+    
+    senha = input("Entre com a senha: ")
+    if senha == "fodase":
+        break
+    elif senha == 'raissinha':
+        print("quase")
+        continue
+    
+    print("fodase")
+
+print("Saí, porra")
+
+
+
+## De 2 em 2
+
+contador = 1
+while contador <= 15:
+
+    if contador % 2 == 0:
+        print(contador)
+
+    contador += 1
