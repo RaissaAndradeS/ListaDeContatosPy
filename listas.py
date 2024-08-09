86feb14e75867b46c1389e874c4ac4a1e514714d
RaissaAndradeS <221035077@aluno.unb.br>
2024-08-09 07:30:37 -0300

    final da aula 2

---
+++ listas.py
@@ -0,0 +1,46 @@
+# Lista vazia
+
+minha_lista = []
+
+# Lista
+
+minha_lista = ['raissa', 'andrade', 25]
+print(minha_lista)
+
+# Acessando os elementos da lista 
+
+minha_lista[0]
+
+minha_lista[1]
+
+minha_lista[2]
+
+# Tamanho da lista
+
+indice = len(minha_lista)-1
+minha_lista[indice]
+
+## Ou 
+
+minha_lista[len(minha_lista)-1]
+
+minha_lista[-1]
+
+
+## Fatiamento de lista
+
+minha_lista[0:2]
+
+# Ultimo elemento 
+
+minha_lista[0:-1]
+
+
+## de dois em dois 
+
+minha_lista[::2] #start:stop:step
+
+# Ao contrario 
+
+minha_lista[::-1]
+
