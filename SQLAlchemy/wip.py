821cbd63123cf34a8bba59b37e6e466b42b017df
RaissaAndradeS <221035077@aluno.unb.br>
2024-08-21 14:07:37 -0300

    sqlalchemy

--- SQLAlchemy/wip.py
+++ SQLAlchemy/wip.py
@@ -1,12 +1,17 @@
-from sqlalchemy import create_engine
+# Apenas um teste com base na live. 
+
+from sqlalchemy import create_engine, text
 
 engine = create_engine(  # Factory
     'sqlite://',
     echo=True
 )
 
-con = engine.connect()
+with engine.connect() as con:
+    with con.begin():
+     sql = text('select id, name, comment from comments')
+     result = con.execute(sql)
 
-print(connection.connection.dbapi_connection)
 
-con.close()
+# print(connection.connection.dbapi_connection)
+# - A: Atomico/ Atomicidade
